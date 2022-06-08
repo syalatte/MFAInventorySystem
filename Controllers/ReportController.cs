dc08d53ee9d43749214c0e46b1015cb2f6ed4721
norarmirani <106641144+norarmirani@users.noreply.github.com>
2022-06-08 20:38:31 +0800

    Report

---
+++ Controllers/ReportController.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MFAInventorySystem.Models;
+using Rotativa;
+
+namespace MFAInventorySystem.Controllers
+{
+    public class ReportController : Controller
+    {
+        private db_mfaEntities db = new db_mfaEntities();
+
+        // GET: Report
+        public ActionResult Index()
+        {
+            return View(db.tb_report.ToList());
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: tb_report/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "r_id,r_name,r_date,r_desc,r_profits,r_capitals")] tb_report tb_report)
+        {
+            if (ModelState.IsValid)
+            {
+                db.tb_report.Add(tb_report);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(tb_report);
+        }
+
+        // GET: Report/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tb_report tb_report = db.tb_report.Find(id);
+            if (tb_report == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tb_report);
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tb_report tb_report = db.tb_report.Find(id);
+            if (tb_report == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tb_report);
+        }
+
+        // POST: tb_report/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(tb_report tb_report)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tb_report).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(tb_report);
+        }
+
+        // GET: tb_report/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tb_report tb_report = db.tb_report.Find(id);
+            if (tb_report == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tb_report);
+        }
+
+        // POST: tb_report/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            tb_report tb_report = db.tb_report.Find(id);
+            db.tb_report.Remove(tb_report);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult GetAll()
+        {
+            var report = db.tb_report.ToList();
+            return View(report);
+        }
+
+        public ActionResult Print()
+        {
+            var q = new ActionAsPdf("GetAll");
+            return q;
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
--- Models/tb_report.cs
+++ Models/tb_report.cs
@@ -11,14 +11,30 @@ namespace MFAInventorySystem.Models
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel;
+    using System.ComponentModel.DataAnnotations;
+
     public partial class tb_report
     {
+        [DisplayName("Report ID")]
         public int r_id { get; set; }
+
+        [DisplayName("Name")]
         public string r_name { get; set; }
-        public string r_desc { get; set; }
+
+        [DisplayName("Date Generated")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public Nullable<System.DateTime> r_date { get; set; }
+
+        [DisplayName("Description")]
+        public string r_desc { get; set; }
+
+        [DisplayName("Profites Gained (RM)")]
+        [DisplayFormat(DataFormatString = "{0:n2}")]
         public Nullable<double> r_profits { get; set; }
+
+        [DisplayName("Capitals (RM)")]
+        [DisplayFormat(DataFormatString = "{0:n2}")]
         public Nullable<double> r_capitals { get; set; }
     }
 }
